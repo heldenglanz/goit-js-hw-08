43ebe430019db3a6f1432b79aa5869278fd0efd3
Heldenglanz <heldenglanz@gmail.com>
2022-08-10 21:48:43 +0300

    ex

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,55 +4,27 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const galleryContainer = document.querySelector('.gallery');
-let instance = '';
 const photoSet = createPhotoMarcup(galleryItems);
 
 galleryContainer.insertAdjacentHTML("beforeend", photoSet);
-galleryContainer.addEventListener('click', clickOnTheImg);
+
 console.log(createPhotoMarcup(galleryItems));
 
 function createPhotoMarcup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
-        return`<div class="gallery__item">
-        <a class="gallery__link"
-        href="${original}">
-            <img 
-        class="gallery__image"
-        src = "${preview}"
-        data-source="${original}"
-        alt = "${description}"
-            /></a></div>`;
+        return `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
     }).join('');
+    
 };
 
-// function clickOnTheImg(e) {
-//     e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') {
-//         return
-//     }
-
-// const large = e.target.dataset.source;
-//     openedModal(large);
-// }
-
-// function openedModal(large) {
-//     instance = simplelightbox.create(
-//         `<img 
-//         src="${large}"
-//     />
-// `);
-//     instance.show();
-//     window.addEventListener('keydown', pressedKey);
-// }
 
 
-function pressedKey(e) {
-    if (e.code === 'Escape') {
-        instance.close();
-        window.removeEventListener('keydown', pressedKey);
-        console.log("Escape");
-}
-}
-// Change code below this line
+let gallery = new SimpleLightbox('.gallery a',{
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+    docClose: true,
+});
 
-// console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
 
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+    id: "vimeo-player",
+    width: 640
+});
+
+// player.on('click', function(data) {
+//     console.log('played the video!');
+
+// });
+
+player.on('click', onPlay);
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time", data);
+    // data is an object containing properties specific to that event
+};
+
+
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(data){
+
+})
