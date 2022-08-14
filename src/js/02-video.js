95bb7747d1b7dd0faf17a327d26bdc86682e1227
Heldenglanz <heldenglanz@gmail.com>
2022-08-14 17:31:28 +0300

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,23 @@
 
 import Player from '@vimeo/player';
-
-const videoEl = document.querySelector('iframe');
-
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const STORAGE_KEY = "videoplayer-current-time";
 const player = new Player("vimeo-player");
 
 player.on('play', function() {
     console.log('played the video!');
+
 });
+
+player.on('timeupdate', throttle(onSaveTime, 500))
+
+
+function onSaveTime({seconds}){
+    localStorage.setItem(STORAGE_KEY, seconds)
+}
+
+const getStorageValue = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(getStorageValue)
+
