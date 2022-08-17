f8c13d3ac5c0dfefcbe7037b207f590d3e64a297
Heldenglanz <heldenglanz@gmail.com>
2022-08-17 22:53:35 +0300

    vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,8 +7,23 @@ const player = new Player(iframe);
 
 
 player.on('timeupdate', throttle(({seconds}) =>{
-    localStorage.setItem(STORAGE_KEY, seconds);
+    localStorage.setItem(STORAGE_KEY, `${seconds}`);
 }, 500));
 
-const comeback = localStorage.getItem(STORAGE_KEY)
-player.setCurrentTime(comeback)
+const localTime = localStorage.getItem(STORAGE_KEY)
+
+player.setCurrentTime(localTime)
+.then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+})
+// .catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
